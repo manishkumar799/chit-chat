aa32e8458f61359bba9befcefecfc26ca4f94bbc
Manish <manishkr9900@gmail.com>
2024-02-13 00:17:39 +0530

    Established socket io connection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:3000',{ transports : ['websocket'] });
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [messages, setMessages] = useState([]);
+    const [input, setInput] = useState('');
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    useEffect(() => {
+        socket.on('message', message => {
+            setMessages([...messages, message]);
+        });
+    }, [messages]);
+
+    const sendMessage = () => {
+        socket.emit('message', input);
+        setInput('');
+    };
+
+    return (
+        <div >
+            <ul>
+                {messages.map((message, index) => (
+                    <li key={index}>{message}</li>
+                ))}
+            </ul>
+            <input
+                type="text"
+                value={input}
+                onChange={e => setInput(e.target.value)}
+                className='bg-[#0077] text-white'
+            />
+            <button onClick={sendMessage}>Send</button>
+        </div>
+    );
 }
 
-export default App
+export default App;
--- server/app.js
+++ server/app.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose");
 const config = require("./config/config.js");
 const routes = require("./app/routes/routes.js");
 const cors = require("cors");
+const morgan = require("morgan");
 const helmet = require("helmet");
+const fs = require("fs");
+const path = require("path");
 const { Server } = require("socket.io");
 
 const { createServer } = require("node:http");
@@ -14,10 +17,23 @@ const server = createServer(app);
 
 const io = new Server(server);
 
-io.on("connection", (socket) => {
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
-    console.log(msg)
+// io.on("connection", (socket) => {
+//   socket.on("chat message", (msg) => {
+//     io.emit("chat message", msg);
+//     console.log(msg);
+//   });
+// });
+
+io.on('connection', socket => {
+  console.log('a user connected');
+
+  socket.on('message', message => {
+      console.log('message:', message);
+      io.emit('message', message);
+  });
+
+  socket.on('disconnect', () => {
+      console.log('user disconnected');
   });
 });
 
@@ -25,30 +41,48 @@ app.use((req, res, next) => {
   req.io = io;
   next();
 });
-app.get("/", (req, res) => {
-  res.sendFile(join(__dirname, "index.html"));
-});
 
-// const swaggerUi = require("swagger-ui-express");
-// const swaggerDocument = require("./swagger-output.json");
-app.use(cors());
-app.use(helmet());
+app.use(cors({
+  origin:["http://localhost:5173", "http://localhost:3000","*"],
+  methods:"GET,POST,PUT,DELETE",
+  credentials:true
+}));
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "access.log"),
+  { flags: "a" }
+);
+app.use(morgan("combined", { stream: accessLogStream }));
+// app.use(
+//   helmet({
+//     crossOriginResourcePolicy: false,
+//   })
+// );
 
 app.set(express.urlencoded({ extended: false }));
 app.set(express.json());
 
-const uri = `mongodb://${config.database.host}:${config.database.port}/${config.database.name}`;
-
-mongoose.connect(uri);
+async function connectToDatabase() {
+  try {
+    await mongoose.connect(`mongodb://${config.database.host}:${config.database.port}/${config.database.name}`);
+    console.log("Mongoose connected to database");
+  } catch (error) {
+    console.error("Mongoose connection error: " + error);
+    process.exit(1); // Exit process if failed to connect to database
+  }
+}
+connectToDatabase();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", routes);
+// app.get("/", async function (req, res) {
+//   res.send("Hello, world!");
+// });
 
 // app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 mongoose.connection.on("connected", () => {
-//   console.log("Mongoose connected to " + uri);
+    // console.log("Mongoose connected to " + uri);
 
   const PORT = process.env.PORT || 3000;
   server.listen(PORT, () => {
